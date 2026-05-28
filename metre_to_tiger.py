8865bfe3afffb63001b972f040cddeae0af825bf
Karthik <reddykarthikdhevr@outlook.com>
2026-05-28 19:48:12 +0530

    Convert metres to tigers
    NOTE: This generator uses Bengal Tigers, not regular African tigers.

---
+++ metre_to_tiger.py
@@ -0,0 +1,11 @@
+import time
+
+metre = float(input('Enter a distance in metres📏: '))
+time.sleep(1)
+
+male_tiger = metre / 2.82
+female_tiger = metre / 2.54
+
+print(f'Your distance is {male_tiger} male tigers.🐯\n')
+time.sleep(0.5)
+print(f'Your distance is {female_tiger} female tigers.🐯\n')
