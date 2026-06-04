32a8eabc496807c07f932ac74736590e960b3de3
Karthik <reddykarthikdhevr@outlook.com>
2026-06-04 17:13:42 +0530

    This slow circle takes 27 hours to complete.
    Patience.. is a virtue.

---
+++ slow_circle.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.color('coral')
+turtle.bgcolor('blue')
+turtle.delay(100)
+turtle.pensize(5)
+turtle.title('Patience.. is a virtue.')
+turtle.speed('slowest')
+for _ in range(1,1000001):
+    turtle.fd(1)
+    turtle.rt(360 / 330)
